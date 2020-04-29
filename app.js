03c66db8000c1954e6d83617a73e0d6eba63bb03
GitHub <noreply@github.com>
2020-04-29 23:42:59 +0000

    6031f4c chore(translations): sync translations from transifex (v29)

--- app.js
+++ app.js
@@ -813,6 +813,7 @@
 	  },
 	  "id": {
 	    "translation": {
+	      "DHIS 2 browser cache cleaner": "DHIS 2 browser cache cleaner",
 	      "Session storage": "Session Storage",
 	      "Local storage": "Local Storage",
 	      "Clear": "Hapus",
